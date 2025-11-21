1b7991351ed4f0b21ac5ca35ebd795f99108c013
Colin Edwards <ColinEdwards8@gmail.com>
2025-11-21 11:46:10 -0800

    through section 12 and update interceptor path name

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,35 +14,20 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/interceptors/serialize.interceptors';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
   constructor(
     private usersService: UsersService,
-    private authService: AuthService
+    private authService: AuthService,
   ) {}
-
-  @Get('/colors/:color')
-  setColor(@Param('color') color: string, @Session() session: any) {
-    session.color = color;
-  }
-
-  @Get('/colors')
-  getColor(@Session() session: any) {
-    return session.color;
-  }
-
-  // @Get('/whoami')
-  // whoAmI(@Session() session: any) {
-  //   return this.usersService.findOne(session.userId)
-  // }
 
   @Get('/whoami')
   @UseGuards(AuthGuard)
@@ -52,20 +37,20 @@ export class UsersController {
 
   @Post('/signout')
   signOut(@Session() session: any) {
-    session.userId = null
+    session.userId = null;
   }
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signup(body.email, body.password);
-    session.userId = user.id
+    session.userId = user.id;
     return user;
   }
 
   @Post('/signin')
   async signin(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signin(body.email, body.password);
-    session.userId = user.id
+    session.userId = user.id;
     return user;
   }
 
@@ -73,9 +58,9 @@ export class UsersController {
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
-        throw new NotFoundException('user not found');
+      throw new NotFoundException('user not found');
     }
-    return user
+    return user;
   }
 
   @Get()
@@ -93,3 +78,99 @@ export class UsersController {
     return this.usersService.update(parseInt(id), body);
   }
 }
+
+// import {
+//   Body,
+//   Controller,
+//   Post,
+//   Get,
+//   Patch,
+//   Delete,
+//   Param,
+//   Query,
+//   NotFoundException,
+//   Session,
+//   UseGuards,
+// } from '@nestjs/common';
+// import { CreateUserDto } from './dtos/create-user.dto';
+// import { UpdateUserDto } from './dtos/update-user.dto';
+// import { UsersService } from './users.service';
+// import { Serialize } from 'src/interceptors/serialize.interceptors';
+// import { UserDto } from './dtos/user.dto';
+// import { AuthService } from './auth.service';
+// import { CurrentUser } from './decorators/current-user.decorator';
+// import { User } from './user.entity';
+// import { AuthGuard } from 'src/guards/auth.guard';
+
+// @Controller('auth')
+// @Serialize(UserDto)
+// export class UsersController {
+//   constructor(
+//     private usersService: UsersService,
+//     private authService: AuthService
+//   ) {}
+
+//   @Get('/colors/:color')
+//   setColor(@Param('color') color: string, @Session() session: any) {
+//     session.color = color;
+//   }
+
+//   @Get('/colors')
+//   getColor(@Session() session: any) {
+//     return session.color;
+//   }
+
+//   // @Get('/whoami')
+//   // whoAmI(@Session() session: any) {
+//   //   return this.usersService.findOne(session.userId)
+//   // }
+
+//   @Get('/whoami')
+//   @UseGuards(AuthGuard)
+//   whoAmI(@CurrentUser() user: User) {
+//     return user;
+//   }
+
+//   @Post('/signout')
+//   signOut(@Session() session: any) {
+//     session.userId = null
+//   }
+
+//   @Post('/signup')
+//   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+//     const user = await this.authService.signup(body.email, body.password);
+//     session.userId = user.id
+//     return user;
+//   }
+
+//   @Post('/signin')
+//   async signin(@Body() body: CreateUserDto, @Session() session: any) {
+//     const user = await this.authService.signin(body.email, body.password);
+//     session.userId = user.id
+//     return user;
+//   }
+
+//   @Get('/:id')
+//   async findUser(@Param('id') id: string) {
+//     const user = await this.usersService.findOne(parseInt(id));
+//     if (!user) {
+//         throw new NotFoundException('user not found');
+//     }
+//     return user
+//   }
+
+//   @Get()
+//   findAllUsers(@Query('email') email: string) {
+//     return this.usersService.find(email);
+//   }
+
+//   @Delete('/:id')
+//   removeUser(@Param('id') id: string) {
+//     return this.usersService.remove(parseInt(id));
+//   }
+
+//   @Patch('/:id')
+//   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+//     return this.usersService.update(parseInt(id), body);
+//   }
+// }
